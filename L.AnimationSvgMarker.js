9ce59dbbd6505bec1e6ad3f5c77bdb5ad1815bfb
Stefan Safranek <stefan@stefan>
2016-09-13 15:37:05 -0700

    util functions

--- L.AnimationSvgMarker.js
+++ L.AnimationSvgMarker.js
@@ -306,7 +306,17 @@ L.AnimationSvgMarker = L.Marker.extend({
                   + ("0" + TIME.getUTCMinutes()).slice(-2) +  ":" 
                   + ("0" + TIME.getUTCSeconds()).slice(-2);
             return d;
-        }
+        },
+
+        /**
+         * Returns a random integer between min (inclusive) and max (inclusive)
+         * Using Math.round() will give you a non-uniform distribution!
+         */
+        getRandomInt: function(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+
+        getDelay: function(x) {var n=(-30*x)+150; return n;}
 
     }
 
